ee77d4b94b88ce7d5b3edc5dd0780bc93de6a6b9
jcjen62 <jcjensen62@gmail.com>
2022-05-01 20:01:18 -0600

    qb card

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <h1>QBR App</h1>
   <helper-para />
   <user-form />
+  <card />
 </template>
 
 <script>
 import HelperPara from './components/HelperPara.vue'
 import UserForm from './components/UserForm.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
     HelperPara,
-    UserForm
+    UserForm,
+    Card
   }
 }
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapperCard">
+      <div class="card">
+        <div class="container">
+          <h4><b>John Doe</b></h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+};
+</script>
+
+<style scoped>
+.wrapperCard {
+  display: flex;
+  flex-wrap: wrap;
+}
+.card {
+  max-width: 23%;
+  flex-basis: 100%;
+  text-align: left;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.5s;
+  margin: 15px 0.5rem 15px 0.5rem;
+  border-radius: 6px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.7);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
